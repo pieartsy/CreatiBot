28cfeb0689cd8fc413fd200329d69b488bd5399b
piearty <piearty@users.noreply.github.com>
2018-12-14 17:39:35 -0500

    updated "playing" status

--- discordbot.py
+++ discordbot.py
@@ -13,7 +13,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
         print("Ready!!!!!")
-
+        await client.change_presence(game=discord.Game(name='$palette for random palette!'))
 @client.event
 async def on_message(message):
         if message.content.startswith('$palette'):
