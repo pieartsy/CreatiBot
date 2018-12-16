1c2ad3a0a0fb27ce5db33c3dc8c5609789d390b9
piearty <piearty@users.noreply.github.com>
2018-12-15 22:13:26 -0500

    got prompts api working

--- discordbot.py
+++ discordbot.py
@@ -3,20 +3,26 @@ import discord
 #imports the palette info
 import palette
 import os.path
+#discord bot token
 from config import TOKEN
 
+#defines filepath again
 filepath = os.getcwd()
 
+#discord client instance
 client = discord.Client()
 
+#when bot boots up, print "Ready!!!!!" to the command line
+#the playing status is "$help for both help
 @client.event
 async def on_ready():
         print("Ready!!!!!")
-        await client.change_presence(game=discord.Game(name='$palette for random palette!'))
+        await client.change_presence(game=discord.Game(name='$help for bot help'))
 @client.event
 async def on_message(message):
         if message.content.startswith('$palette'):
                 palette.palettegen()
                 await client.send_file(message.channel, os.path.join(filepath, "palette.png"))
+
 
 client.run(TOKEN)
