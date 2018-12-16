bffbc0496510f64a77111a081adea69df010c814
piearty <piearty@users.noreply.github.com>
2018-12-15 22:25:15 -0500

    got prompts working in discordbot

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,9 @@
 #discord bot time!
 import discord
-#imports the palette info
+#imports the palette and prompts info
 import palette
+import prompts
+#to join paths
 import os.path
 #discord bot token
 from config import TOKEN
@@ -18,11 +20,20 @@ client = discord.Client()
 async def on_ready():
         print("Ready!!!!!")
         await client.change_presence(game=discord.Game(name='$help for bot help'))
+#on message $palette, runs palettegen() from the palette.py file, and sends the image to the channel
+#where command was sent
 @client.event
 async def on_message(message):
         if message.content.startswith('$palette'):
                 palette.palettegen()
                 await client.send_file(message.channel, os.path.join(filepath, "palette.png"))
+#on message $prompt, runs promptgen from the prompts.py file and sends the prompt the channel
+#where command was sent
+@client.event
+async def on_message(message):
+        if message.content.startswith('$prompt'):
+                prompt = prompts.promptgen()
+                await client.send_message(message.channel, prompt)
 
-
+#runs the bot
 client.run(TOKEN)
--- prompts.py
+++ prompts.py
@@ -5,21 +5,22 @@ from config import REDDIT_ID, REDDIT_SECRET, REDDIT_PWD, REDDIT_USERNAME
 #to choose from a random prompt
 import random
 
-#reddit instance
-reddit = praw.Reddit(client_id= REDDIT_ID,
-                     client_secret= REDDIT_SECRET,
-                     password= REDDIT_PWD,
-                     user_agent='CreatiBot',
-                     username= REDDIT_USERNAME)
+def promptgen():
+    #reddit instance
+    reddit = praw.Reddit(client_id= REDDIT_ID,
+                        client_secret= REDDIT_SECRET,
+                        password= REDDIT_PWD,
+                        user_agent='CreatiBot',
+                        username= REDDIT_USERNAME)
 
-#gets subreddit instance
-subreddit = reddit.subreddit('writingprompts')
+    #gets subreddit instance
+    subreddit = reddit.subreddit('writingprompts')
 
-#newest 50 writing prompts
-new50prompts = []
+    #newest 50 writing prompts
+    new50prompts = []
 
-#for submission in the Writing Prompts flair
-for submission in subreddit.search('flair:"Writing Prompt"', limit=50):
-    new50prompts.append(submission.title.replace("[WP]", ''))
+    #for submission in the Writing Prompts flair
+    for submission in subreddit.search('flair:"Writing Prompt"', limit=50):
+        new50prompts.append(submission.title.replace("[WP]", ''))
 
-prompt = random.choice(new50prompts)
+    return random.choice(new50prompts)
